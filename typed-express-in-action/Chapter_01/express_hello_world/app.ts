cd58d36aafb14fc21b10bf38bc5f5a6ae857f1ce
smkamranqadri <smkamranqadri@yahoo.com>
2015-11-07 14:21:48 +0500

    Express5 Syntax

--- typed-express-in-action/Chapter_01/express_hello_world/app.ts
+++ typed-express-in-action/Chapter_01/express_hello_world/app.ts
@@ -8,6 +8,11 @@ app.get("/", (request : express.Request, response: express.Response) => {
   response.send("Hello world!");
 });
 
+//New Syntax for Espress5
+app.get("/test", (request : express.Request, response: express.Response) => {
+  response.status(200).send("Hello world!").
+});
+
 app.listen(3000, () => {
   console.log("Express app started on port 3000.");
 });
